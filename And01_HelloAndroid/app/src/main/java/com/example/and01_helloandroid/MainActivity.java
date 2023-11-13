fc70c5c9c2f583da4ce6778a82e41068df52eb44
YmKim <77032646+youngmoon525@users.noreply.github.com>
2023-11-13 13:05:18 +0900

    aa
    a

---
+++ And01_HelloAndroid/app/src/main/java/com/example/and01_helloandroid/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.and01_helloandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String str = new String();//ctrl + p
+        //코드 자동 완성 : alt + insert , 마우스 오른쪽 클릭 -> 자동완성(Generate)
+
+    }
+}
---
+++ And02_IntoLayoutConst/app/src/main/java/com/example/and02_intolayoutconst/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.and02_intolayoutconst;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //setContentView(R.layout.activity_main);
+
+    }
+}

bd8fa7a24e4d33c1e63c8e5b5de4c083fbab851a
YmKim <77032646+youngmoon525@users.noreply.github.com>
2023-12-14 15:04:39 +0900

    a
    a

--- Project02_Last/app/src/main/java/com/example/project02_last/member/LoginActivity.java
+++ Project02_Last/app/src/main/java/com/example/project02_last/member/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.project02_last.member;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -18,7 +19,9 @@ import com.example.project02_last.R;
 import com.example.project02_last.common.CommonConn;
 import com.example.project02_last.databinding.ActivityLoginBinding;
 import com.google.gson.Gson;
+import com.kakao.sdk.auth.model.OAuthToken;
 import com.kakao.sdk.common.KakaoSdk;
+import com.kakao.sdk.user.UserApiClient;
 import com.navercorp.nid.NaverIdLoginSDK;
 import com.navercorp.nid.oauth.NidOAuthLogin;
 import com.navercorp.nid.oauth.OAuthLoginCallback;
@@ -27,6 +30,9 @@ import com.navercorp.nid.profile.data.NidProfileResponse;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function2;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -37,7 +43,7 @@ public class LoginActivity extends AppCompatActivity {
         //NaverIdLoginSDK.initialize(context, {OAUTH_CLIENT_ID}, {OAUTH_CLIENT_SECRET}, {OAUTH_CLIENT_NAME})
         //values -> key.xml ( gitignore )
         //getString(R.string.cliendId)
-        NaverIdLoginSDK.INSTANCE.initialize(this , "WUN9kjcUtJXyW54ywFzX" , "ZF27K29u6V" , "Project02_Last");
+        NaverIdLoginSDK.INSTANCE.initialize(this , getString(R.string.CLIENT_ID_NAVER),  getString(R.string.CLIENT_SEC_NAVER) , "Project02_Last");
 
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
@@ -53,37 +59,70 @@ public class LoginActivity extends AppCompatActivity {
 //            Log.d("테스트", "onCreate: " + isResult + data);
 //        });
         binding.btnLogin.setOnClickListener(v->{
-            CommonConn conn = new CommonConn(this,"login.me");
-            conn.addParamMap("user_id" , binding.edtId.getText().toString());
-            conn.addParamMap("user_pw" , binding.edtPw.getText().toString());
-            conn.onExcute((isResult, data) -> {
-                //서버가 응답을 함.(또는 오류)
-                //1.toJson ( 내가 StringJson으로 데이터를 줘야함 )
-                //2.fromJson ( StringJson을 받아와서 내가 원하는 형태로 바꿈 )
-                Log.d("로그인", "onCreate: " + data);
-                MemberVO vo = new Gson().fromJson(data , MemberVO.class);
-                if(vo == null){
-                    Toast.makeText(this, "아이디 또는 패스워드 틀림", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                Intent intent = new Intent(this, MainActivity.class);
-                startActivity(intent);
-                //finish(); 로그인->Main ( 뒤로가기 눌렀을 때 다시 로그인 화면이 나옴 )
-
-
-            });
+           login(binding.edtId.getText().toString() , binding.edtPw.getText().toString());
         });
 
+
+        binding.imgvKakaoLogin.setOnClickListener(v-> {
+            kakaoLogin(this);
+
+        });
         naverLogin();
-        kakaoLogin();
-        getHashKey();
+
+        //집에서 실행하면 해시키가 다름.-> 문서에 등록해줘야함.
+        //getHashKey();
     }
 
-    public void kakaoLogin(){
+
+
+    //kakao는 key값을 우리에게 제공x ( email , mobile )
+    public void kakaoLogin(Context context){
         //KakaoSdk.init(this, "{NATIVE_APP_KEY}")
         KakaoSdk.INSTANCE.init(this, "6b31da7eff82c2b2b197a964fbc79efe");
+        //카카오톡 설치 여부. true->설치가 되어있으면 앱으로 인증(권장)
+        //                 false->웹뷰(크롬)를 이용해서 인증
+
+
+        Function2<OAuthToken, Throwable, Unit> callback = new Function2<OAuthToken, Throwable, Unit>() {
+            @Override
+            public Unit invoke(OAuthToken oAuthToken, Throwable error) {
+                if(error == null){
+                    Log.d("카카오", "invoke: " + oAuthToken.getAccessToken());
+                    //kakaotalk->하단 메뉴 5 더보기-> 우측 상단 설정 -> 카카오 계정 -> 연결된서비스관리->외부 서비스
+                    UserApiClient.getInstance().me( (user, throwable) ->{
+                        if(throwable==null){
+                            Log.d("카카오", "invoke(정보): " + user.getKakaoAccount().getProfile().getNickname());
+                            Log.d("카카오", "invoke(정보): " + user.getKakaoAccount().getProfile().getProfileImageUrl());
+
+                        }else{
+                            Log.d("카카오", "invoke(요류): " + throwable.getMessage());
+
+                        }
+
+                        return null;
+                    });
+
+
+                }else{
+                    Log.d("카카오", "invoke: " + error.getMessage());
+                }
+
+                return null;
+            }
+        };
+
+        if(UserApiClient.getInstance().isKakaoTalkLoginAvailable(context)){
+            Log.d("카카오", "kakaoLogin: 카카오톡이 설치 되어 있음->APP ");
+            UserApiClient.getInstance().loginWithKakaoTalk(context, callback);
+
+        }else{
+            Log.d("카카오", "kakaoLogin: 카카오톡이 설치 X->WEB ");
+            UserApiClient.getInstance().loginWithKakaoAccount(context,callback);
+        }
     }
 
+
+    //naver는 email을 제공-> database social,id(email)
     public void naverLogin(){
         binding.buttonOAuthLoginImg.setOAuthLogin(new OAuthLoginCallback() {
             @Override
@@ -96,6 +135,9 @@ public class LoginActivity extends AppCompatActivity {
                         //Social ( key ) 네이버 아이디 ..카카오톡 아이디 등등.
                         //id ( key ) <- naver,kakao... kym@naver.com
                         Log.d("네이버", "onSuccess: " + nidProfileResponse.getProfile().getEmail());
+
+                        login(nidProfileResponse.getProfile().getEmail() , null);
+
                         Log.d("네이버", "onSuccess: " + nidProfileResponse.getProfile().getMobile());
                         Log.d("네이버", "onSuccess: " + nidProfileResponse.getProfile().getName());
                         Log.d("네이버", "onSuccess: " + nidProfileResponse.getProfile().getProfileImage());
@@ -149,5 +191,30 @@ public class LoginActivity extends AppCompatActivity {
             }
         }
     }
+
+    //일반 로그인
+    public void login(String user_id , String user_pw){
+        CommonConn conn = new CommonConn(this,"login.me");
+        conn.addParamMap("user_id" , user_id);
+        conn.addParamMap("user_pw" , user_pw);
+        conn.onExcute((isResult, data) -> {
+            //서버가 응답을 함.(또는 오류)
+            //1.toJson ( 내가 StringJson으로 데이터를 줘야함 )
+            //2.fromJson ( StringJson을 받아와서 내가 원하는 형태로 바꿈 )
+            Log.d("로그인", "onCreate: " + data);
+            MemberVO vo = new Gson().fromJson(data , MemberVO.class);
+            if(vo == null){
+                Toast.makeText(this, "아이디 또는 패스워드 틀림", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            //finish(); 로그인->Main ( 뒤로가기 눌렀을 때 다시 로그인 화면이 나옴 )
+
+
+        });
+    }
+
+
 
 }
